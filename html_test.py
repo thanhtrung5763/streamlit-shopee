c89e9bdfd76e2ad2eb44f358bf824c9d0e84df00
thanhtrung5763 <thanhtrung5763@gmail.com>
2023-03-26 22:07:23 +0700

    css

---
+++ html_test.py
@@ -0,0 +1,263 @@
+POINT = '''
+<div class="section-contents"><img src="https://flush-image-prod.s3.ap-northeast-2.amazonaws.com/01GAJ695CS0E2Q5HVVVNDAD5FG/P026110.webp" alt=""><span class="btn-detection" data-color="blue" data-active="true" style="top: 65%; left: 51%;"><span class="btn-detection-inner"></span></span></div>
+'''
+PRODUCT_INFO = '''
+    <section class="info-section section-product-info">
+    <header>
+        <h3 class="title">Product Information</h3>
+    </header>
+    <div class="section-contents">
+        <div class="info-grouped">
+            <div class="grouped-header">
+                <h3 class="title">Basic Information</h3>
+            </div>
+            <dl class="list-info">
+                <div class="info-item">
+                    <dt>Product ID</dt>
+                    <dd>SB99724</dd>
+                </div>
+                <div class="info-item">
+                    <dt>Product Name</dt>
+                    <dd style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><span
+                            class="empty">-</span></dd>
+                </div>
+                <div class="info-item">
+                    <dt>Product No.</dt>
+                    <dd style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><span
+                            class="empty">-</span></dd>
+                </div>
+            </dl>
+            <div class="grouped-header">
+                <h3 class="title">Key Attribute Tags</h3>
+            </div>
+            <dl class="list-info">
+                <div class="info-item">
+                    <dt>Category &gt; Item</dt>
+                    <dd>
+                        <div class="select-category-item">Knitwear &gt; Turtleneck</div>
+                    </dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Length</dt>
+                    <dd>Normal</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Sleeve Length</dt>
+                    <dd>Long Sleeves</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Neckline</dt>
+                    <dd>Turtle Neck</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Fit</dt>
+                    <dd>Normal</dd>
+                </div>
+                <div class="info-item"></div>
+            </dl>
+        </div>
+        <div class="section-footer">
+            <form>
+            <button class="product-link" formaction="https://www.wconcept.co.kr/Product/301034416" formtarget="_blank">
+                <i class="icon left"><svg class="ui-control ui-icon" viewBox="0 0 24 24" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <path fill="#f8f9fa" d="M18.9 15c-1.428 0-2.664 0.78-3.336 1.92l-6.816-3.576c0.156-0.42 0.252-0.876 0.252-1.344s-0.096-0.924-0.252-1.344l6.816-3.576c0.684 1.14 1.92 1.92 3.336 1.92 2.148 0 3.9-1.752 3.9-3.9s-1.752-3.9-3.9-3.9c-2.148 0-3.9 1.752-3.9 3.9 0 0.084 0.024 0.156 0.024 0.228l-7.284 3.816c-0.696-0.648-1.62-1.044-2.64-1.044-2.148 0-3.9 1.752-3.9 3.9s1.752 3.9 3.9 3.9c1.020 0 1.944-0.408 2.64-1.044l7.284 3.816c0 0.084-0.024 0.156-0.024 0.228 0 2.148 1.752 3.9 3.9 3.9s3.9-1.752 3.9-3.9c0-2.148-1.752-3.9-3.9-3.9zM18.9 3c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1zM5.1 14.1c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1zM18.9 21c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1z">
+                                    </path>
+                </svg></i>
+                                <span class="txt">Produck Link</span></button>
+            </form>
+        </div>
+    </div>
+</section>
+'''
+
+TABLE = '''<section class="info-section" style="margin-top: 0px; padding-top: 40px;">
+    <header>
+        <h3 class="title">Product Tags<span class="bar"></span>
+            <div class="ui-control ui-select-control undefined  type-attribute  outline  outline-0">
+                <div class="select-control-container css-b62m3t-container"><span
+                        id="react-select-ai-attribute-live-region" class="css-7pg0cj-a11yText"></span><span
+                        aria-live="polite" aria-atomic="false" aria-relevant="additions text"
+                        class="css-7pg0cj-a11yText"></span>
+                </div>
+            </div>
+        </h3>
+    </header><input type="hidden" name="categoryGroup" value="CLOTHING">
+    <div class="section-contents">
+        <div class="info-grouped">
+            <div class="grouped-header">
+                <h3 class="title">Key Attribute Tags</h3>
+            </div>
+            <dl class="list-info">
+                <div class="info-item">
+                    <dt>Category &gt; Item</dt>
+                    <dd>
+                        <div class="select-category-item">Knitwear &gt; Turtleneck</div>
+                    </dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Length</dt>
+                    <dd>Normal</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Sleeve Length</dt>
+                    <dd>Long Sleeves</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Neckline</dt>
+                    <dd>Turtle Neck</dd>
+                </div>
+                <div class="info-item">
+                    <dt data-dirty="false">Fit</dt>
+                    <dd>Normal</dd>
+                </div>
+                <div class="info-item"></div>
+            </dl>
+            <div class="grouped-header">
+                <h3 class="title">Detailed Attribute Tags (Select up to 5 tags)</h3>
+            </div>
+            <dl class="list-info">
+                <div class="info-item">
+                    <dt>Color</dt>
+                    <dd>White</dd>
+                </div>
+                <div class="info-item">
+                    <dt>Print</dt>
+                    <dd>Solid</dd>
+                </div>
+                <div class="info-item">
+                    <dt>Texture</dt>
+                    <dd>Knit</dd>
+                </div>
+                <div class="info-item">
+                    <dt>Detail</dt>
+                    <dd>Drop Shoulder</dd>
+                </div>
+                <div class="info-item">
+                    <dt>Style</dt>
+                    <dd>Casual</dd>
+                </div>
+                <div class="info-item"></div>
+            </dl>
+        </div>
+    </div>
+</section>'''
+
+
+def product_info(data):
+
+    product = f'''
+        <section class="info-section section-product-info">
+        <header>
+            <h3 class="title">Product Information</h3>
+        </header>
+        <div class="section-contents">
+            <div class="info-grouped">
+                <div class="grouped-header">
+                    <h3 class="title">Basic Information</h3>
+                </div>
+                <dl class="list-info">
+                    <div class="info-item">
+                        <dt>Product ID</dt>
+                        <dd>{data['item_id']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Product Name</dt>
+                        <dd style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{data['name']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Product No.</dt>
+                        <dd style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><span
+                                class="empty">-</span></dd>
+                    </div>
+                </dl>
+            </div>
+            <div class="section-footer">
+                <form>
+                <button class="product-link" formaction={data['url']} formtarget="_blank">
+                    <i class="icon left"><svg class="ui-control ui-icon" viewBox="0 0 24 24" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <path fill="#f8f9fa" d="M18.9 15c-1.428 0-2.664 0.78-3.336 1.92l-6.816-3.576c0.156-0.42 0.252-0.876 0.252-1.344s-0.096-0.924-0.252-1.344l6.816-3.576c0.684 1.14 1.92 1.92 3.336 1.92 2.148 0 3.9-1.752 3.9-3.9s-1.752-3.9-3.9-3.9c-2.148 0-3.9 1.752-3.9 3.9 0 0.084 0.024 0.156 0.024 0.228l-7.284 3.816c-0.696-0.648-1.62-1.044-2.64-1.044-2.148 0-3.9 1.752-3.9 3.9s1.752 3.9 3.9 3.9c1.020 0 1.944-0.408 2.64-1.044l7.284 3.816c0 0.084-0.024 0.156-0.024 0.228 0 2.148 1.752 3.9 3.9 3.9s3.9-1.752 3.9-3.9c0-2.148-1.752-3.9-3.9-3.9zM18.9 3c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1zM5.1 14.1c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1zM18.9 21c-1.152 0-2.1-0.948-2.1-2.1s0.948-2.1 2.1-2.1c1.152 0 2.1 0.948 2.1 2.1s-0.948 2.1-2.1 2.1z">
+                                        </path>
+                    </svg></i>
+                                    <span class="txt">Produck Link</span></button>
+                </form>
+            </div>
+        </div>
+    </section>
+    '''
+    return product
+
+
+def product_tag(data):
+
+    product = f'''<section class="info-section" style="margin-top: 0px; padding-top: 40px;">
+        <header>
+            <h3 class="title">Product Tags<span class="bar"></span>
+                <div class="ui-control ui-select-control undefined  type-attribute  outline  outline-0">
+                    <div class="select-control-container css-b62m3t-container"><span
+                            id="react-select-ai-attribute-live-region" class="css-7pg0cj-a11yText"></span><span
+                            aria-live="polite" aria-atomic="false" aria-relevant="additions text"
+                            class="css-7pg0cj-a11yText"></span>
+                    </div>
+                </div>
+            </h3>
+        </header><input type="hidden" name="categoryGroup" value="CLOTHING">
+        <div class="section-contents">
+            <div class="info-grouped">
+                <div class="grouped-header">
+                    <h3 class="title">Key Attribute Tags</h3>
+                </div>
+                <dl class="list-info">
+                    <div class="info-item">
+                        <dt>Category &gt; Item</dt>
+                        <dd>
+                            <div class="select-category-item">{data['category']} &gt; {data['sub_category']}</div>
+                        </dd>
+                    </div>
+                    <div class="info-item">
+                        <dt data-dirty="false">Length</dt>
+                        <dd>{data['length']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt data-dirty="false">Sleeve Length</dt>
+                        <dd>{data['sleeve_length']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt data-dirty="false">Neckline</dt>
+                        <dd>{data['neck_line']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt data-dirty="false">Fit</dt>
+                        <dd>{data['fit']}</dd>
+                    </div>
+                    <div class="info-item"></div>
+                </dl>
+                <div class="grouped-header">
+                    <h3 class="title">Detailed Attribute Tags (Select up to 5 tags)</h3>
+                </div>
+                <dl class="list-info">
+                    <div class="info-item">
+                        <dt>Color</dt>
+                        <dd>{data['tag_color']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Print</dt>
+                        <dd>{data['print']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Texture</dt>
+                        <dd>{data['texture']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Detail</dt>
+                        <dd>{data['detail']}</dd>
+                    </div>
+                    <div class="info-item">
+                        <dt>Style</dt>
+                        <dd>{data['look']}</dd>
+                    </div>
+                    <div class="info-item"></div>
+                </dl>
+            </div>
+        </div>
+    </section>'''
+    return product
